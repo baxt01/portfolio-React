8e912886a326f74addb22e09413a984795825572
Michael Baxter <baxt01@hotmail.com>
2024-03-11 23:25:56 +0000

    I have completed the styling for the project gallery

--- src/components/pages/ProjectGallery.jsx
+++ src/components/pages/ProjectGallery.jsx
@@ -6,13 +6,26 @@ import './ProjectGallery.css';
 
 export default function ProjectGallery() {
   return (
-    <div>
-      <h1>Project Gallery</h1>
-      {projectsData.map((project, index) => (
-        <Link key={index} to={`/project/${index}`}>
-          {project.title}<br />
-        </Link>
-      ))}
+    <div className='container text-left'>
+        <div className='row'>
+            <div className='col-lg-3 col-sm-0'>
+                </div>
+              <div className='col-lg-8 col-sm-4'>
+                <h1 className='ms-5 title'>  Project Gallery</h1>
+                <div className='section text-center px-5 py-5 text-center'>
+                <div className="projects">
+                {projectsData.map((project, index) => (
+                  <Link key={index} to={`/project/${index}`}>
+                    {project.title}<br />
+                  </Link>
+                ))}
+                  </div>
+                </div>
+           </div>
+            <div className='col-lg-1 col-sm-0'>
+        </div>
     </div>
-  );
+    </div>
+    
+    );
 }
